f5f622c74227430789e5616dc3f72782447af8a0
DiegoRosalesHall <diegogerardorhall2@Gmail.com>
2024-03-01 14:17:40 -0600

    Si se puede

--- src/lab7p2_diegorosales/AdmProducto.java
+++ src/lab7p2_diegorosales/AdmProducto.java
@@ -1,13 +1,83 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package lab7p2_diegorosales;
 
-/**
- *
- * @author Diego
- */
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class AdmProducto {
+    ArrayList<Producto> productos = new ArrayList<>();
+    File archivo = null;
+
+    public AdmProducto() {
+    }
+    
+    public AdmProducto(String path){
+        archivo = new File(path);
+    }
+
+    public ArrayList<Producto> getProductos() {
+        return productos;
+    }
+
+    public void setProductos(ArrayList<Producto> productos) {
+        this.productos = productos;
+    }
+
+    public File getArchivo() {
+        return archivo;
+    }
+
+    public void setArchivo(File archivo) {
+        this.archivo = archivo;
+    }
+    
+    public void cargarArchivo(){
+        Scanner sc = null;
+        productos = new ArrayList<>();
+        if(archivo.exists()){
+            try {
+                sc = new Scanner(archivo);
+                sc.useDelimiter(",");
+                while (sc.hasNext()) {
+                    productos.add(new Producto(sc.nextInt(),
+                                    sc.nextInt(),
+                                    sc.nextInt(),
+                            sc.nextInt(),
+                            sc.next(),
+                            sc.nextDouble()
+                                 ) 
+                    );
+                }
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+        }
+    }
+    
+   public void escribirArchivo() throws IOException {
+        FileWriter fw = null;
+        BufferedWriter bw = null;
+        try {
+            fw = new FileWriter(archivo, false);
+            bw = new BufferedWriter(fw);
+            for (Producto t : productos) {
+                
+                bw.write(t.getId()+",");
+                bw.write(t.getName()+",");
+                bw.write(t.getCategory()+",");
+                bw.write(t.getPrice()+",");
+                bw.write(t.getAisle()+",");
+                bw.write(t.getBin()+",");
+            }
+            bw.flush();
+        } catch (Exception ex) {
+        }
+        bw.close();
+        fw.close();
+    }
     
 }
